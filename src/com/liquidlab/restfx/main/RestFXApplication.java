943d13df0d9715bcaec4a003b725896fa5ff46b6
Thamizh Arasu <zarub2k@gmail.com>
2014-04-13 15:53:49 +0530

    main file added

---
+++ src/com/liquidlab/restfx/main/RestFXApplication.java
@@ -0,0 +1,18 @@
+package com.liquidlab.restfx.main;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RestFXApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setScene(new Scene(null, 300, 275));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
