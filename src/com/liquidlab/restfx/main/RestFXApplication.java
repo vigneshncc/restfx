1b26ed0ca51a4a1962deb33a7cce6e5894433003
Thamizh Arasu <zarub2k@gmail.com>
2014-04-14 00:15:41 +0530

    un-used imports are removed

--- src/com/liquidlab/restfx/main/RestFXApplication.java
+++ src/com/liquidlab/restfx/main/RestFXApplication.java
@@ -20,7 +20,6 @@ import javafx.application.Application;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
